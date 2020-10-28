2ddb4e03501a0741cae5c4190fc8790dcc18d596
SalvioliAx <65875242+SalvioliAx@users.noreply.github.com>
2020-10-28 11:46:51 +0100

    finished
    finished

---
+++ js/owl-carousel/setup.js
@@ -0,0 +1,50 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})  
+// Get the videoPlayer
+
+var link = document.querySelector('.myModalVideo');// get iframe class 
+var link = document.querySelector('#myModalVideo');// get iframe id
+var video_src = link.getAttribute('src');
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  $('.modal-content').children('iframe').attr('src', ''); // set iframe parent div value null 
+  $('.modal-content').children('iframe').attr('src', video_src);// set iframe src again it works perfect
+ 
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
